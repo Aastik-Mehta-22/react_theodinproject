8e5f7f5be94308da5dddcfa9bbbcd8f99bc758a5
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-08-24 14:59:28 +0530

    learn evaluated expression

--- firstBasicProject/src/Greeting.jsx
+++ firstBasicProject/src/Greeting.jsx
@@ -1,6 +1,6 @@
 function Greeting(){
-
-    return <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>;
+    const username = "Aastik";
+    return <h1>&quot;I swear by my pretty floral bonnet, I will end you {username}.&quot;</h1>;
 }
 
 export default Greeting;
